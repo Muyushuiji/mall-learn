2424ba24c34914abca207de0f520999d40c9e221
hxl <huxinlong00@126.com>
2023-02-01 11:36:23 +0800

    代码生成器+部分swagger注释

---
+++ src/main/java/com/mall/tiny/MallApplication.java
@@ -0,0 +1,17 @@
+package com.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: untitled
+ * @description: 测试用
+ * @author: hxl
+ * @create: 2022-10-17 16:32
+ **/
+@SpringBootApplication
+public class MallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplication.class,args);
+    }
+}
